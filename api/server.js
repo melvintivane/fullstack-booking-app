63e957c1f00b2520c68707fe376b4801114c981b
Mello47 <melvinshuster47@gmail.com>
2023-09-28 22:39:14 +0200

    Add middleware to handle errors throughout the aplication

--- api/server.js
+++ api/server.js
@@ -27,6 +27,17 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
 app.listen(8080, () => {
   connect();
   console.log(`DB Connected. \nServer Running at: http://localhost:8080`);
