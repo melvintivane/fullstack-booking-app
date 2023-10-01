e54e65b6587b595bd2121e8384196d6cd1c894f8
Mello47 <melvinshuster47@gmail.com>
2023-10-01 18:38:28 +0200

    Add count by city in getHotels

--- api/controllers/hotelController.js
+++ api/controllers/hotelController.js
@@ -7,7 +7,7 @@ export const createHotel = async (req, res, next) => {
     const createdHotel = await newHotel.save();
     res.status(200).json(createdHotel);
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
@@ -16,7 +16,7 @@ export const getHotel = async (req, res, next) => {
     const hotel = await Hotel.findById(req.params.id);
     res.status(200).json(hotel);
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
@@ -25,9 +25,26 @@ export const getHotels = async (req, res, next) => {
     const hotels = await Hotel.find();
     res.status(200).json(hotels);
   } catch (error) {
+    next(error);
+  }
+};
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+
+    res.status(200).json(list)
+  } catch (error) {
     next(error)
   }
 };
+
+export const countByType = async (req, res, next) => {};
 
 export const updateHotel = async (req, res, next) => {
   try {
@@ -38,7 +55,7 @@ export const updateHotel = async (req, res, next) => {
     );
     res.status(200).json(updatedHotel);
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
@@ -47,6 +64,6 @@ export const deleteHotel = async (req, res, next) => {
     await Hotel.findByIdAndDelete(req.params.id);
     res.status(200).json("Hotel has been deleted.");
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
