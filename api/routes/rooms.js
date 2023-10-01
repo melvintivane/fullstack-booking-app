2f55e4eaaa594716cc1db7485d11c55b942449f0
Mello47 <melvinshuster47@gmail.com>
2023-10-01 18:32:27 +0200

    Add room management routes

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,23 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+//CREATE ROOM
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//GET ROOM
+router.get("/:id", getRoom);
+
+//GET ROOMS
+router.get("/", getRooms);
+
+//UPDATE ROOM
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE ROOM
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
 
 
 export default router;
