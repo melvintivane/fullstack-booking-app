88262c2d8df1880435b43a3e9b18b31972d25f91
melvintivane <melvinshuster47@gmail.com>
2023-10-21 08:03:40 +0200

    Add local storage for user state

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
-  user: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   loading: false,
   error: null,
 };
@@ -45,6 +45,10 @@ export const AuthReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
 
   return (
     <AuthContext.Provider
