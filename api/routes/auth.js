70527c2f6a74387d287b51b16dfe467112d133be
melvintivane <melvinshuster47@gmail.com>
2023-10-21 08:02:10 +0200

    Change login route to use POST method

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,6 +4,6 @@ import { login, register } from "../controllers/authController.js";
 const router = express.Router();
 
 router.post("/register", register);
-router.get("/login", login);
+router.post("/login", login);
 
 export default router;
