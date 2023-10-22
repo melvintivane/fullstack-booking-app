11e5a52303a100080150e5911da3da1bb1027aa1
melvintivane <melvinshuster47@gmail.com>
2023-10-22 14:31:22 +0200

    Add reservation functionality in the Hotel component

--- client/src/pages/Hotel/Hotel.jsx
+++ client/src/pages/Hotel/Hotel.jsx
@@ -15,14 +15,20 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import useFetch from "../../hooks/useFetch";
 import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import Reserve from "../../components/Reserve/Reserve";
 
 const Hotel = () => {
   const [slideNumber, setSlideNumber] = useState(0);
   const id = location.pathname.split("/")[2];
   const [open, setOpen] = useState(false);
-
+  const [openModal, setOpenmodal] = useState(false);
 
   const { dates, options } = useContext(SearchContext);
+  const {user} = useContext(AuthContext);
+
+  const navigate = useNavigate();
 
   const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
   function dayDifference(date1, date2) {
@@ -76,6 +82,14 @@ const Hotel = () => {
     setSlideNumber(newSlideNumber);
   };
 
+  const handleClick = ()=> {
+    if (user) {
+      setOpenmodal(true)
+    } else {
+      navigate("/login")
+    }
+  }
+
   return (
     <div>
       <Navbar />
@@ -111,7 +125,7 @@ const Hotel = () => {
             </div>
           )}
           <div className="hotelWrapper">
-            <button className="bookNow">Reserve or Book Now!</button>
+            <button className="bookNow"  onClick={handleClick}>Reserve or Book Now!</button>
             <h1 className="hotelTitle">{data.name}</h1>
             <div className="hotelAddress">
               <FontAwesomeIcon icon={faLocationDot} />
@@ -151,7 +165,7 @@ const Hotel = () => {
                 <h2>
                   <b>${days * data.cheapestPrice * options.room}</b> ({days} nights)
                 </h2>
-                <button>Reserve or Book Now!</button>
+                <button onClick={handleClick}>Reserve or Book Now!</button>
               </div>
             </div>
           </div>
@@ -159,6 +173,7 @@ const Hotel = () => {
           <Footer />
         </div>
       )}
+      {openModal && <Reserve setOpen={setOpenmodal} hotelId={id}/>}
     </div>
   );
 };
