183ad20e86d1c1885c32445bececeda676bc3b1f
melvintivane <melvinshuster47@gmail.com>
2023-10-29 14:12:57 +0200

    Add endpoint to get hotel rooms

--- api/controllers/hotelController.js
+++ api/controllers/hotelController.js
@@ -1,4 +1,5 @@
 import Hotel from "../models/Hotel.js";
+import Room from "../models/Room.js";
 
 export const createHotel = async (req, res, next) => {
   const newHotel = new Hotel(req.body);
@@ -98,5 +99,19 @@ export const deleteHotel = async (req, res, next) => {
     res.status(200).json("Hotel has been deleted.");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getHotelRooms = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    const list = await Promise.all(
+      hotel.rooms.map((room) => {
+        return Room.findById(room);
+      })
+    );
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
   }
 };
